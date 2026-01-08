a1ef4c376c362bc1620c9e67c06af63eae422ae7
[NilaBeast] <[manika.basak1977@gmail.com]>
2026-01-08 12:29:03 +0530

    Server updated

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,6 +3,10 @@ const { sendContactMail } = require("../controllers/contactController");
 
 const router = express.Router();
 
+router.get("/contact", (req, res) => {
+  res.send("Contact API is working. Use POST to send mail.");
+});
+
 router.post("/contact", sendContactMail);
 
 module.exports = router;
